108e579c587e69d5f12a231fdf6bef3229cb43d6
marat <marat19955@gmail.com>
2023-03-23 21:01:35 +0300

    HW completed

---
+++ src/Authentication.java
@@ -0,0 +1,39 @@
+public class Authentication implements AuthService {
+    public static boolean checkLogin(String login) {
+        if (login.length() > 20) {
+            throw new WrongLoginException("Логин должен быть не более 20 символов" +
+                    ", а введено " + login.length());
+        }
+        if (!login.matches("^[a-zA-Z0-9_]*$")) {
+            throw new WrongLoginException("Логин должен содержать только " +
+                    "латинские буквы, цифры и знак подчеркивания");
+        }
+        return true;
+    }
+
+    public static boolean checkPassword(String password) {
+        if (password.length() >= 20) {
+            throw new WrongPasswordException("Пароль должен быть строго меньше 20 символов" +
+                    ", а введено " + password.length());
+        }
+        if (!password.matches("^[a-zA-Z0-9_]*$")) {
+            throw new WrongPasswordException("Пароль должен содержать только " +
+                    "латинские буквы, цифры и знак подчеркивания");
+        }
+        return true;
+    }
+
+    public static boolean comparePasswordWithConfirmPassword(String password, String confirmPassword) {
+        if (!password.equals(confirmPassword)) {
+            throw new WrongPasswordException("Параметры password и confirmPassword " +
+                    "должны быть равны");
+        }
+        return true;
+    }
+
+    @Override
+    public String operationAuth(String login, String password, String confirmPassword) {
+        return null;
+    }
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(Authentication.checkLogin("java_skyprv1"));
+        System.out.println(Authentication.checkPassword("D_1hWidjxbqjkdb"));
+        System.out.println(Authentication.comparePasswordWithConfirmPassword("D_1hWiKjjP_9", "D_1hWiKjj_9"));
     }
 }
