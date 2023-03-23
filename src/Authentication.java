9ccaee7659cd8562564652fe005ceb629466aed9
marat <marat19955@gmail.com>
2023-03-23 22:08:53 +0300

    HW completed

--- src/Authentication.java
+++ src/Authentication.java
@@ -1,34 +1,53 @@
 public class Authentication implements AuthService {
     public static boolean checkLogin(String login) {
-        if (login.length() > 20) {
-            throw new WrongLoginException("Логин должен быть не более 20 символов" +
-                    ", а введено " + login.length());
+        try {
+
+
+            if (login.length() > 20) {
+                throw new WrongLoginException("Логин должен быть не более 20 символов" +
+                        ", а введено " + login.length());
+            }
+            if (!login.matches("^[a-zA-Z0-9_]*$")) {
+                throw new WrongLoginException("Логин должен содержать только " +
+                        "латинские буквы, цифры и знак подчеркивания");
+            }
+            return true;
+        } catch (WrongLoginException e) {
+            System.out.println(e.getMessage());
+            return false;
         }
-        if (!login.matches("^[a-zA-Z0-9_]*$")) {
-            throw new WrongLoginException("Логин должен содержать только " +
-                    "латинские буквы, цифры и знак подчеркивания");
-        }
-        return true;
     }
 
     public static boolean checkPassword(String password) {
-        if (password.length() >= 20) {
-            throw new WrongPasswordException("Пароль должен быть строго меньше 20 символов" +
-                    ", а введено " + password.length());
+        try {
+
+            if (password.length() >= 20) {
+                throw new WrongPasswordException("Пароль должен быть строго меньше 20 символов" +
+                        ", а введено " + password.length());
+            }
+            if (!password.matches("^[a-zA-Z0-9_]*$")) {
+                throw new WrongPasswordException("Пароль должен содержать только " +
+                        "латинские буквы, цифры и знак подчеркивания");
+            }
+            return true;
+        } catch (WrongPasswordException e) {
+            System.out.println(e.getMessage());
+            return false;
         }
-        if (!password.matches("^[a-zA-Z0-9_]*$")) {
-            throw new WrongPasswordException("Пароль должен содержать только " +
-                    "латинские буквы, цифры и знак подчеркивания");
-        }
-        return true;
     }
 
     public static boolean comparePasswordWithConfirmPassword(String password, String confirmPassword) {
-        if (!password.equals(confirmPassword)) {
-            throw new WrongPasswordException("Параметры password и confirmPassword " +
-                    "должны быть равны");
+        try {
+
+            if (!password.equals(confirmPassword)) {
+                throw new WrongPasswordException("Параметры password и confirmPassword " +
+                        "должны быть равны");
+            }
+            return true;
+        } catch (WrongPasswordException e) {
+            System.out.println(e.getMessage());
+            return false;
         }
-        return true;
     }
 
     @Override
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Authentication.checkLogin("java_skyprv1"));
-        System.out.println(Authentication.checkPassword("D_1hWidjxbqjkdb"));
-        System.out.println(Authentication.comparePasswordWithConfirmPassword("D_1hWiKjjP_9", "D_1hWiKjj_9"));
+        System.out.println(Authentication.checkLogin("java_skhayprogo"));
+        System.out.println(Authentication.checkPassword("D_1Adjxbqjkdb"));
+        System.out.println(Authentication.comparePasswordWithConfirmPassword("D_1hWiKjj_9", "D_1hWiKjjP_9"));
     }
 }
